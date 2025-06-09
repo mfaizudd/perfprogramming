6ff4d11fd24d997df3c2c6dc076b5808712a1948
Muhammad Faizud Daroin <m@faizud.net>
2025-06-09 11:23:54 +0700

    feat: immediate to register

--- disassembler/src/main.rs
+++ disassembler/src/main.rs
@@ -1,6 +1,6 @@
 use clap::{Parser, Subcommand, arg, command};
 use phf::phf_map;
-use std::{fs::File, io::Read};
+use std::{fs::File, io::{Read, Write}, slice::Iter};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -20,12 +20,13 @@ enum Commands {
     AsciiBytes,
 }
 
-static OP: phf::Map<[u8; 1], fn(&[u8])> = phf_map! {
-    [0b100010] => mov
+static OP: phf::Map<[u8; 1], fn(&u8, &mut Iter<u8>)> = phf_map! {
+    [0b100010] => mov,
+    [0b1011] => im_mov,
 };
 
 static REG: phf::Map<[u8; 1], &str> = phf_map! {
-    [0b000u8] => "al",
+    [0b000] => "al",
     [0b001] => "cl",
     [0b010] => "dl",
     [0b011] => "bl",
@@ -62,41 +63,59 @@ fn main() {
 
 fn ascii(buf: &[u8], file: &str) {
     println!("ASCII Binary representation of {}", file);
-    for b in buf.chunks(2) {
-        if b.len() > 1 {
-            println!("{:#010b} {:#010b}", b[0], b[1]);
-        } else {
-            println!("{:#010b}", b[0]);
-        }
+    for b in buf {
+        println!("{:#010b} ", b);
     }
 }
 
 fn asm(buf: &[u8], file: &str) {
     println!("; generated from {}", file);
     println!("bits 16");
-    for b in buf.chunks(2) {
-        // println!("; {:#08b}", b[0]);
-        // println!("; {:#08b}", b[1]);
-        let op = OP.get(&[b[0] >> 2]).expect("OpCode not supported");
-        op(b);
+    let mut buf = buf.iter();
+    while let Some(b) = buf.next() {
+        let op = OP
+            .get(&[b >> 4])
+            .or_else(|| OP.get(&[b >> 2]))
+            .expect("OpCode not supported");
+        op(b, &mut buf);
     }
+    // for b in buf.chunks(2) {
+    //     let op = OP.get(&[b[0] >> 2]).expect("OpCode not supported");
+    //     op(b);
+    // }
 }
 
-fn mov(b: &[u8]) {
-    let d = b[0] & 0b10 != 0;
-    let w = b[0] & 0b1 != 0;
-    let map = if w { &WREG } else { &REG };
+fn mov(b: &u8, buf: &mut Iter<u8>) {
+    let d = b & 0b10 != 0;
+    let w = b & 0b1 != 0;
+    let reg = if w { &WREG } else { &REG };
+
+    let b = buf.next().expect("No second byte");
     let src = if !d {
-        map.get(&[((b[1] & 0b111000) >> 3)])
+        reg.get(&[((b & 0b111000) >> 3)])
             .expect("Register not found")
     } else {
-        map.get(&[(b[1] & 0b111)]).expect("Register not found")
+        reg.get(&[(b & 0b111)]).expect("Register not found")
     };
     let dst = if d {
-        map.get(&[((b[1] & 0b111000) >> 3)])
+        reg.get(&[((b & 0b111000) >> 3)])
             .expect("Register not found")
     } else {
-        map.get(&[(b[1] & 0b111)]).expect("Register not found")
+        reg.get(&[(b & 0b111)]).expect("Register not found")
     };
     println!("mov {dst}, {src}");
+}
+
+fn im_mov(b: &u8, buf: &mut Iter<u8>) {
+    let w = b & 0b1000 != 0;
+    let reg = if w { &WREG } else { &REG };
+    let dst = reg.get(&[b & 0b111]).expect("Register not found");
+    let data1 = buf.next().expect("No immediate data followed");
+    let data2 = if w {
+        buf.next().expect("w is set, but no more data is provided")
+    } else {
+        &0b0
+    };
+    let data = u16::from_le_bytes([*data1, *data2]);
+    println!("mov {dst}, {data}",);
 }
